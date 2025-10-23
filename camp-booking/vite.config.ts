e3d450dc635abb7e0f04f38a0eb5122c1f4811a2
Zack Modica <zmodica@gmail.com>
2025-10-22 21:21:29 -0400

    adding github pages related changes

--- camp-booking/vite.config.ts
+++ camp-booking/vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue({ customElement: true })],
+  base: '/camp-booking/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
